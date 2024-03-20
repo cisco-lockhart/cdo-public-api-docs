62e23fb6f431a1975580e2e8bf57988b5c7807f0
Tal Hazi <thazi@cisco.com>
2024-03-20 17:02:12 +0200

    set pypi creds

--- scripts/src/publish_python_sdk_command.sh
+++ scripts/src/publish_python_sdk_command.sh
@@ -1,8 +1,12 @@
+# Retrieve the secret value from AWS Secrets Manager and update .pypirc
+secret_value=$(aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-west-2:107042026245:secret:jenkins-pypi-credentials-mD4NdK --query SecretString --output text)
+sed -i "s/\"CHANGE_ME\"/$secret_value/" .pypirc
+
 cd cdo-sdk/python
 pip install -r requirements.txt
 pip install wheel
 pip install twine
-python3 setup.py sdist bdist_wheel
 
-# need to set pypi creds
+# Prepare to publish & Publish to PyPI
+python3 setup.py sdist bdist_wheel
 twine upload dist/*
