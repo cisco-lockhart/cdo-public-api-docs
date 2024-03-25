382eec47525d5b4dd0661acea64947f98ab691ce
CDO Jenkins <cdo.ops@cisco.com>
2024-03-25 12:08:20 +0000

    Update OpenAPI.yaml from staging

--- cdo-sdk/python/setup.py
+++ cdo-sdk/python/setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "cdo-sdk-python"
-VERSION = "1.0.14"
+VERSION = "1.0.15"
 PYTHON_REQUIRES = ">=3.7"
 REQUIRES = [
     "urllib3 >= 1.25.3, < 2.1.0",
