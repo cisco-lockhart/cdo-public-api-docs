bd4361f94571f99530db4486937708379399f91c
Tal Hazi <thazi@cisco.com>
2024-05-05 16:32:31 +0300

    Update OpenAPI.yaml from staging

--- scripts/src/publish_python_sdk_command.sh
+++ scripts/src/publish_python_sdk_command.sh
@@ -3,8 +3,7 @@ scripts/cli update-sdk-description
 
 secret_arn=arn:aws:secretsmanager:us-west-2:107042026245:secret:jenkins-pypi-credentials-mD4NdK
 echo -n "$(yellow Retrieving the secret value from AWS Secrets Manager)... "
-# secret_value=$(aws secretsmanager get-secret-value --secret-id $secret_arn --query SecretString --output text)
-secret_value=pypi-AgEIcHlwaS5vcmcCJDdlYzM4ZTVkLTExMTUtNGVhZi1iZWJjLWE4MzhjODU5MjVkZQACFlsxLFsiY2RvLXNkay1weXRob24iXV0AAixbMixbIjIzZWEyMmNiLWM1MDQtNGJjMC05NWFjLWNiOTA4MDdkNzZhYyJdXQAABiCbLPx9Y8wSqob6hGNZrr6E5O1HfuserkMBLZBNH5TGsg
+secret_value=$(aws secretsmanager get-secret-value --secret-id $secret_arn --query SecretString --output text)
 
 package_name=$(grep -Eo 'NAME = "[^"]+"' cdo-sdk/python/setup.py | cut -d'"' -f2)
 current_version=$(grep -Eo "VERSION = \"[0-9]+\.[0-9]+\.[0-9]+\"" cdo-sdk/python/setup.py | cut -d'"' -f2)
@@ -16,7 +15,6 @@ echo -n "$(yellow Creating the Wheel and Source)... "
 python3 setup.py sdist bdist_wheel
 
 echo -n "$(yellow Publishing to PyPI)... "
-# /home/jenkins/.local/bin/twine upload --username __token__ --password $secret_value dist/${package_name}-${current_version}.tar.gz dist/${package_name//-/_}-${current_version}-py3-none-any.whl
-twine upload --username __token__ --password $secret_value dist/${package_name}-${current_version}.tar.gz dist/${package_name//-/_}-${current_version}-py3-none-any.whl
+/home/jenkins/.local/bin/twine upload --username __token__ --password $secret_value dist/${package_name}-${current_version}.tar.gz dist/${package_name//-/_}-${current_version}-py3-none-any.whl
 
 unset secret_value
