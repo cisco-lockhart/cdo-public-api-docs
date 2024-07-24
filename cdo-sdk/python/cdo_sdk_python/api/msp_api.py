7bf2e138163c6511f9423578118a63e674401f65
CDO Jenkins <cdo.ops@cisco.com>
2024-07-24 13:26:53 +0000

    Update OpenAPI.yaml from staging

--- cdo-sdk/python/cdo_sdk_python/api/msp_api.py
+++ cdo-sdk/python/cdo_sdk_python/api/msp_api.py
@@ -18,7 +18,9 @@ from typing import Any, Dict, List, Optional, Tuple, Union
 from typing_extensions import Annotated
 
 from pydantic import Field, StrictStr
+from typing import List
 from typing_extensions import Annotated
+from cdo_sdk_python.models.active_directory_group_create_or_update_input import ActiveDirectoryGroupCreateOrUpdateInput
 from cdo_sdk_python.models.cdo_transaction import CdoTransaction
 from cdo_sdk_python.models.msp_add_tenant_input import MspAddTenantInput
 from cdo_sdk_python.models.msp_add_users_to_tenant_input import MspAddUsersToTenantInput
@@ -41,6 +43,295 @@ class MSPApi:
         if api_client is None:
             api_client = ApiClient.get_default()
         self.api_client = api_client
+
+
+    @validate_call
+    def add_active_directory_groups_to_tenant_in_msp_portal(
+        self,
+        tenant_uid: Annotated[StrictStr, Field(description="Unique identifier of the tenant to which the user groups will be added.")],
+        active_directory_group_create_or_update_input: List[ActiveDirectoryGroupCreateOrUpdateInput],
+        _request_timeout: Union[
+            None,
+            Annotated[StrictFloat, Field(gt=0)],
+            Tuple[
+                Annotated[StrictFloat, Field(gt=0)],
+                Annotated[StrictFloat, Field(gt=0)]
+            ]
+        ] = None,
+        _request_auth: Optional[Dict[StrictStr, Any]] = None,
+        _content_type: Optional[StrictStr] = None,
+        _headers: Optional[Dict[StrictStr, Any]] = None,
+        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
+    ) -> CdoTransaction:
+        """Add Active Directory Groups to CDO tenant in MSP Portal
+
+        This is an asynchronous operation to add a list of Active Directory Groups to a tenant associated with the MSP Portal.
+
+        :param tenant_uid: Unique identifier of the tenant to which the user groups will be added. (required)
+        :type tenant_uid: str
+        :param active_directory_group_create_or_update_input: (required)
+        :type active_directory_group_create_or_update_input: List[ActiveDirectoryGroupCreateOrUpdateInput]
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :type _request_timeout: int, tuple(int, int), optional
+        :param _request_auth: set to override the auth_settings for an a single
+                              request; this effectively ignores the
+                              authentication in the spec for a single request.
+        :type _request_auth: dict, optional
+        :param _content_type: force content-type for the request.
+        :type _content_type: str, Optional
+        :param _headers: set to override the headers for a single
+                         request; this effectively ignores the headers
+                         in the spec for a single request.
+        :type _headers: dict, optional
+        :param _host_index: set to override the host_index for a single
+                            request; this effectively ignores the host_index
+                            in the spec for a single request.
+        :type _host_index: int, optional
+        :return: Returns the result object.
+        """ # noqa: E501
+
+        _param = self._add_active_directory_groups_to_tenant_in_msp_portal_serialize(
+            tenant_uid=tenant_uid,
+            active_directory_group_create_or_update_input=active_directory_group_create_or_update_input,
+            _request_auth=_request_auth,
+            _content_type=_content_type,
+            _headers=_headers,
+            _host_index=_host_index
+        )
+
+        _response_types_map: Dict[str, Optional[str]] = {
+            '202': "CdoTransaction",
+            '500': "CommonApiError",
+        }
+        response_data = self.api_client.call_api(
+            *_param,
+            _request_timeout=_request_timeout
+        )
+        response_data.read()
+        return self.api_client.response_deserialize(
+            response_data=response_data,
+            response_types_map=_response_types_map,
+        ).data
+
+
+    @validate_call
+    def add_active_directory_groups_to_tenant_in_msp_portal_with_http_info(
+        self,
+        tenant_uid: Annotated[StrictStr, Field(description="Unique identifier of the tenant to which the user groups will be added.")],
+        active_directory_group_create_or_update_input: List[ActiveDirectoryGroupCreateOrUpdateInput],
+        _request_timeout: Union[
+            None,
+            Annotated[StrictFloat, Field(gt=0)],
+            Tuple[
+                Annotated[StrictFloat, Field(gt=0)],
+                Annotated[StrictFloat, Field(gt=0)]
+            ]
+        ] = None,
+        _request_auth: Optional[Dict[StrictStr, Any]] = None,
+        _content_type: Optional[StrictStr] = None,
+        _headers: Optional[Dict[StrictStr, Any]] = None,
+        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
+    ) -> ApiResponse[CdoTransaction]:
+        """Add Active Directory Groups to CDO tenant in MSP Portal
+
+        This is an asynchronous operation to add a list of Active Directory Groups to a tenant associated with the MSP Portal.
+
+        :param tenant_uid: Unique identifier of the tenant to which the user groups will be added. (required)
+        :type tenant_uid: str
+        :param active_directory_group_create_or_update_input: (required)
+        :type active_directory_group_create_or_update_input: List[ActiveDirectoryGroupCreateOrUpdateInput]
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :type _request_timeout: int, tuple(int, int), optional
+        :param _request_auth: set to override the auth_settings for an a single
+                              request; this effectively ignores the
+                              authentication in the spec for a single request.
+        :type _request_auth: dict, optional
+        :param _content_type: force content-type for the request.
+        :type _content_type: str, Optional
+        :param _headers: set to override the headers for a single
+                         request; this effectively ignores the headers
+                         in the spec for a single request.
+        :type _headers: dict, optional
+        :param _host_index: set to override the host_index for a single
+                            request; this effectively ignores the host_index
+                            in the spec for a single request.
+        :type _host_index: int, optional
+        :return: Returns the result object.
+        """ # noqa: E501
+
+        _param = self._add_active_directory_groups_to_tenant_in_msp_portal_serialize(
+            tenant_uid=tenant_uid,
+            active_directory_group_create_or_update_input=active_directory_group_create_or_update_input,
+            _request_auth=_request_auth,
+            _content_type=_content_type,
+            _headers=_headers,
+            _host_index=_host_index
+        )
+
+        _response_types_map: Dict[str, Optional[str]] = {
+            '202': "CdoTransaction",
+            '500': "CommonApiError",
+        }
+        response_data = self.api_client.call_api(
+            *_param,
+            _request_timeout=_request_timeout
+        )
+        response_data.read()
+        return self.api_client.response_deserialize(
+            response_data=response_data,
+            response_types_map=_response_types_map,
+        )
+
+
+    @validate_call
+    def add_active_directory_groups_to_tenant_in_msp_portal_without_preload_content(
+        self,
+        tenant_uid: Annotated[StrictStr, Field(description="Unique identifier of the tenant to which the user groups will be added.")],
+        active_directory_group_create_or_update_input: List[ActiveDirectoryGroupCreateOrUpdateInput],
+        _request_timeout: Union[
+            None,
+            Annotated[StrictFloat, Field(gt=0)],
+            Tuple[
+                Annotated[StrictFloat, Field(gt=0)],
+                Annotated[StrictFloat, Field(gt=0)]
+            ]
+        ] = None,
+        _request_auth: Optional[Dict[StrictStr, Any]] = None,
+        _content_type: Optional[StrictStr] = None,
+        _headers: Optional[Dict[StrictStr, Any]] = None,
+        _host_index: Annotated[StrictInt, Field(ge=0, le=0)] = 0,
+    ) -> RESTResponseType:
+        """Add Active Directory Groups to CDO tenant in MSP Portal
+
+        This is an asynchronous operation to add a list of Active Directory Groups to a tenant associated with the MSP Portal.
+
+        :param tenant_uid: Unique identifier of the tenant to which the user groups will be added. (required)
+        :type tenant_uid: str
+        :param active_directory_group_create_or_update_input: (required)
+        :type active_directory_group_create_or_update_input: List[ActiveDirectoryGroupCreateOrUpdateInput]
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :type _request_timeout: int, tuple(int, int), optional
+        :param _request_auth: set to override the auth_settings for an a single
+                              request; this effectively ignores the
+                              authentication in the spec for a single request.
+        :type _request_auth: dict, optional
+        :param _content_type: force content-type for the request.
+        :type _content_type: str, Optional
+        :param _headers: set to override the headers for a single
+                         request; this effectively ignores the headers
+                         in the spec for a single request.
+        :type _headers: dict, optional
+        :param _host_index: set to override the host_index for a single
+                            request; this effectively ignores the host_index
+                            in the spec for a single request.
+        :type _host_index: int, optional
+        :return: Returns the result object.
+        """ # noqa: E501
+
+        _param = self._add_active_directory_groups_to_tenant_in_msp_portal_serialize(
+            tenant_uid=tenant_uid,
+            active_directory_group_create_or_update_input=active_directory_group_create_or_update_input,
+            _request_auth=_request_auth,
+            _content_type=_content_type,
+            _headers=_headers,
+            _host_index=_host_index
+        )
+
+        _response_types_map: Dict[str, Optional[str]] = {
+            '202': "CdoTransaction",
+            '500': "CommonApiError",
+        }
+        response_data = self.api_client.call_api(
+            *_param,
+            _request_timeout=_request_timeout
+        )
+        return response_data.response
+
+
+    def _add_active_directory_groups_to_tenant_in_msp_portal_serialize(
+        self,
+        tenant_uid,
+        active_directory_group_create_or_update_input,
+        _request_auth,
+        _content_type,
+        _headers,
+        _host_index,
+    ) -> RequestSerialized:
+
+        _host = None
+
+        _collection_formats: Dict[str, str] = {
+            'ActiveDirectoryGroupCreateOrUpdateInput': '',
+        }
+
+        _path_params: Dict[str, str] = {}
+        _query_params: List[Tuple[str, str]] = []
+        _header_params: Dict[str, Optional[str]] = _headers or {}
+        _form_params: List[Tuple[str, str]] = []
+        _files: Dict[str, str] = {}
+        _body_params: Optional[bytes] = None
+
+        # process the path parameters
+        if tenant_uid is not None:
+            _path_params['tenantUid'] = tenant_uid
+        # process the query parameters
+        # process the header parameters
+        # process the form parameters
+        # process the body parameter
+        if active_directory_group_create_or_update_input is not None:
+            _body_params = active_directory_group_create_or_update_input
+
+
+        # set the HTTP header `Accept`
+        _header_params['Accept'] = self.api_client.select_header_accept(
+            [
+                'application/json'
+            ]
+        )
+
+        # set the HTTP header `Content-Type`
+        if _content_type:
+            _header_params['Content-Type'] = _content_type
+        else:
+            _default_content_type = (
+                self.api_client.select_header_content_type(
+                    [
+                        'application/json'
+                    ]
+                )
+            )
+            if _default_content_type is not None:
+                _header_params['Content-Type'] = _default_content_type
+
+        # authentication setting
+        _auth_settings: List[str] = [
+        ]
+
+        return self.api_client.param_serialize(
+            method='POST',
+            resource_path='/v1/msp/tenants/{tenantUid}/users/groups',
+            path_params=_path_params,
+            query_params=_query_params,
+            header_params=_header_params,
+            body=_body_params,
+            post_params=_form_params,
+            files=_files,
+            auth_settings=_auth_settings,
+            collection_formats=_collection_formats,
+            _host=_host,
+            _request_auth=_request_auth
+        )
+
+
 
 
     @validate_call
@@ -658,9 +949,6 @@ class MSPApi:
 
         _response_types_map: Dict[str, Optional[str]] = {
             '202': "CdoTransaction",
-            '400': "CommonApiError",
-            '401': "AuthenticationError",
-            '403': "CommonApiError",
             '500': "CommonApiError",
         }
         response_data = self.api_client.call_api(
@@ -733,9 +1021,6 @@ class MSPApi:
 
         _response_types_map: Dict[str, Optional[str]] = {
             '202': "CdoTransaction",
-            '400': "CommonApiError",
-            '401': "AuthenticationError",
-            '403': "CommonApiError",
             '500': "CommonApiError",
         }
         response_data = self.api_client.call_api(
@@ -808,9 +1093,6 @@ class MSPApi:
 
         _response_types_map: Dict[str, Optional[str]] = {
             '202': "CdoTransaction",
-            '400': "CommonApiError",
-            '401': "AuthenticationError",
-            '403': "CommonApiError",
             '500': "CommonApiError",
         }
         response_data = self.api_client.call_api(
@@ -876,7 +1158,6 @@ class MSPApi:
 
         # authentication setting
         _auth_settings: List[str] = [
-            'bearerAuth'
         ]
 
         return self.api_client.param_serialize(
--- cdo-sdk/python/docs/MSPApi.md
+++ cdo-sdk/python/docs/MSPApi.md
@@ -4,11 +4,84 @@ All URIs are relative to *https://edge.us.cdo.cisco.com/api/rest*
 
 Method | HTTP request | Description
 ------------- | ------------- | -------------
+[**add_active_directory_groups_to_tenant_in_msp_portal**](MSPApi.md#add_active_directory_groups_to_tenant_in_msp_portal) | **POST** /v1/msp/tenants/{tenantUid}/users/groups | Add Active Directory Groups to CDO tenant in MSP Portal
 [**add_msp_tenant**](MSPApi.md#add_msp_tenant) | **POST** /v1/msp/tenants | Add Tenant to MSP Portal (API token)
 [**add_tenant_to_msp_portal**](MSPApi.md#add_tenant_to_msp_portal) | **POST** /v1/msp/tenants/{tenantUid} | Add tenant to MSP Portal
 [**add_users_to_tenant_in_msp_portal**](MSPApi.md#add_users_to_tenant_in_msp_portal) | **POST** /v1/msp/tenants/{tenantUid}/users | Add users to CDO tenant in MSP Portal
 [**create_tenant**](MSPApi.md#create_tenant) | **POST** /v1/msp/tenants/create | Create CDO Tenant
 
+
+# **add_active_directory_groups_to_tenant_in_msp_portal**
+> CdoTransaction add_active_directory_groups_to_tenant_in_msp_portal(tenant_uid, active_directory_group_create_or_update_input)
+
+Add Active Directory Groups to CDO tenant in MSP Portal
+
+This is an asynchronous operation to add a list of Active Directory Groups to a tenant associated with the MSP Portal.
+
+### Example
+
+
+```python
+import cdo_sdk_python
+from cdo_sdk_python.models.active_directory_group_create_or_update_input import ActiveDirectoryGroupCreateOrUpdateInput
+from cdo_sdk_python.models.cdo_transaction import CdoTransaction
+from cdo_sdk_python.rest import ApiException
+from pprint import pprint
+
+# Defining the host is optional and defaults to https://edge.us.cdo.cisco.com/api/rest
+# See configuration.py for a list of all supported configuration parameters.
+configuration = cdo_sdk_python.Configuration(
+    host = "https://edge.us.cdo.cisco.com/api/rest"
+)
+
+
+# Enter a context with an instance of the API client
+with cdo_sdk_python.ApiClient(configuration) as api_client:
+    # Create an instance of the API class
+    api_instance = cdo_sdk_python.MSPApi(api_client)
+    tenant_uid = 'tenant_uid_example' # str | Unique identifier of the tenant to which the user groups will be added.
+    active_directory_group_create_or_update_input = [cdo_sdk_python.ActiveDirectoryGroupCreateOrUpdateInput()] # List[ActiveDirectoryGroupCreateOrUpdateInput] | 
+
+    try:
+        # Add Active Directory Groups to CDO tenant in MSP Portal
+        api_response = api_instance.add_active_directory_groups_to_tenant_in_msp_portal(tenant_uid, active_directory_group_create_or_update_input)
+        print("The response of MSPApi->add_active_directory_groups_to_tenant_in_msp_portal:\n")
+        pprint(api_response)
+    except Exception as e:
+        print("Exception when calling MSPApi->add_active_directory_groups_to_tenant_in_msp_portal: %s\n" % e)
+```
+
+
+
+### Parameters
+
+
+Name | Type | Description  | Notes
+------------- | ------------- | ------------- | -------------
+ **tenant_uid** | **str**| Unique identifier of the tenant to which the user groups will be added. | 
+ **active_directory_group_create_or_update_input** | [**List[ActiveDirectoryGroupCreateOrUpdateInput]**](ActiveDirectoryGroupCreateOrUpdateInput.md)|  | 
+
+### Return type
+
+[**CdoTransaction**](CdoTransaction.md)
+
+### Authorization
+
+No authorization required
+
+### HTTP request headers
+
+ - **Content-Type**: application/json
+ - **Accept**: application/json
+
+### HTTP response details
+
+| Status code | Description | Response headers |
+|-------------|-------------|------------------|
+**202** | CDO Transaction object that can be used to track the status of the operation. |  -  |
+**500** | Internal server error. |  -  |
+
+[[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to Model list]](../README.md#documentation-for-models) [[Back to README]](../README.md)
 
 # **add_msp_tenant**
 > StatusInfo add_msp_tenant(msp_add_tenant_input)
@@ -185,7 +258,6 @@ This is an asynchronous operation to add a list of users to a tenant associated 
 
 ### Example
 
-* Bearer (JWT) Authentication (bearerAuth):
 
 ```python
 import cdo_sdk_python
@@ -200,15 +272,6 @@ configuration = cdo_sdk_python.Configuration(
     host = "https://edge.us.cdo.cisco.com/api/rest"
 )
 
-# The client must configure the authentication and authorization parameters
-# in accordance with the API server security policy.
-# Examples for each auth method are provided below, use the example that
-# satisfies your auth use case.
-
-# Configure Bearer authorization (JWT): bearerAuth
-configuration = cdo_sdk_python.Configuration(
-    access_token = os.environ["BEARER_TOKEN"]
-)
 
 # Enter a context with an instance of the API client
 with cdo_sdk_python.ApiClient(configuration) as api_client:
@@ -242,7 +305,7 @@ Name | Type | Description  | Notes
 
 ### Authorization
 
-[bearerAuth](../README.md#bearerAuth)
+No authorization required
 
 ### HTTP request headers
 
@@ -254,9 +317,6 @@ Name | Type | Description  | Notes
 | Status code | Description | Response headers |
 |-------------|-------------|------------------|
 **202** | CDO Transaction object that can be used to track the status of the operation. |  -  |
-**400** | Invalid input provided. Check the response for details. |  -  |
-**401** | Request not authorized. |  -  |
-**403** | User does not have sufficient privileges to perform this operation. |  -  |
 **500** | Internal server error. |  -  |
 
 [[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to Model list]](../README.md#documentation-for-models) [[Back to README]](../README.md)
