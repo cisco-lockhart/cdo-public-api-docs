f800c3f663a365ca2ab40424d606a0545d7cce66
CDO Jenkins <cdo.ops@cisco.com>
2024-08-22 10:10:48 +0000

    Update OpenAPI.yaml from staging

--- cdo-sdk/python/cdo_sdk_python/__init__.py
+++ cdo-sdk/python/cdo_sdk_python/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "1.2.68"
+__version__ = "1.2.69"
 
 # import apis into sdk package
 from cdo_sdk_python.api.ai_assistant_api import AIAssistantApi
--- cdo-sdk/python/cdo_sdk_python/api_client.py
+++ cdo-sdk/python/cdo_sdk_python/api_client.py
@@ -88,7 +88,7 @@ class ApiClient:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'OpenAPI-Generator/1.2.68/python'
+        self.user_agent = 'OpenAPI-Generator/1.2.69/python'
         self.client_side_validation = configuration.client_side_validation
 
     def __enter__(self):
