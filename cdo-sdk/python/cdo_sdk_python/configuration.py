826d1f848493c76d1b7ec3d94cf3f1111110a56c
CDO Jenkins <cdo.ops@cisco.com>
2024-10-31 11:19:32 +0000

    Update OpenAPI.yaml from staging

--- cdo-sdk/python/cdo_sdk_python/configuration.py
+++ cdo-sdk/python/cdo_sdk_python/configuration.py
@@ -380,7 +380,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.5.0\n"\
-               "SDK Package Version: 1.2.270".\
+               "SDK Package Version: 1.2.271".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
