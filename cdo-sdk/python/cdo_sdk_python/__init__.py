4493c534020dc77372d2c7c5d544010204f4b27e
CDO Jenkins <cdo.ops@cisco.com>
2024-11-12 17:30:24 +0000

    Update OpenAPI.yaml from staging

--- cdo-sdk/python/cdo_sdk_python/__init__.py
+++ cdo-sdk/python/cdo_sdk_python/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "1.2.388"
+__version__ = "1.2.389"
 
 # import apis into sdk package
 from cdo_sdk_python.api.ai_assistant_api import AIAssistantApi
--- cdo-sdk/python/cdo_sdk_python/configuration.py
+++ cdo-sdk/python/cdo_sdk_python/configuration.py
@@ -380,7 +380,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.5.0\n"\
-               "SDK Package Version: 1.2.388".\
+               "SDK Package Version: 1.2.389".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
