bd1342619403c019faf93b451c675c71585220db
CDO Jenkins <cdo.ops@cisco.com>
2024-11-12 10:15:01 +0000

    Update OpenAPI.yaml from staging

--- cdo-sdk/python/cdo_sdk_python/configuration.py
+++ cdo-sdk/python/cdo_sdk_python/configuration.py
@@ -380,7 +380,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 1.5.0\n"\
-               "SDK Package Version: 1.2.380".\
+               "SDK Package Version: 1.2.381".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
--- cdo-sdk/python/cdo_sdk_python/models/redirect_view_servlet_context_session_cookie_config.py
+++ cdo-sdk/python/cdo_sdk_python/models/redirect_view_servlet_context_session_cookie_config.py
@@ -31,10 +31,10 @@ class RedirectViewServletContextSessionCookieConfig(BaseModel):
     name: Optional[StrictStr] = None
     path: Optional[StrictStr] = None
     comment: Optional[StrictStr] = None
-    max_age: Optional[StrictInt] = Field(default=None, alias="maxAge")
     secure: Optional[StrictBool] = None
     domain: Optional[StrictStr] = None
-    __properties: ClassVar[List[str]] = ["httpOnly", "name", "path", "comment", "maxAge", "secure", "domain"]
+    max_age: Optional[StrictInt] = Field(default=None, alias="maxAge")
+    __properties: ClassVar[List[str]] = ["httpOnly", "name", "path", "comment", "secure", "domain", "maxAge"]
 
     model_config = ConfigDict(
         populate_by_name=True,
@@ -91,9 +91,9 @@ class RedirectViewServletContextSessionCookieConfig(BaseModel):
             "name": obj.get("name"),
             "path": obj.get("path"),
             "comment": obj.get("comment"),
-            "maxAge": obj.get("maxAge"),
             "secure": obj.get("secure"),
-            "domain": obj.get("domain")
+            "domain": obj.get("domain"),
+            "maxAge": obj.get("maxAge")
         })
         return _obj
 
