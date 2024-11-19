dde14dfe30a3e45a7b6602c9fb08bb4358e380b3
CDO Jenkins <cdo.ops@cisco.com>
2024-11-19 12:46:19 +0000

    Update OpenAPI.yaml from staging

--- cdo-sdk/python/cdo_sdk_python/models/redirect_view_servlet_context_jsp_config_descriptor_taglibs_inner.py
+++ cdo-sdk/python/cdo_sdk_python/models/redirect_view_servlet_context_jsp_config_descriptor_taglibs_inner.py
@@ -27,9 +27,9 @@ class RedirectViewServletContextJspConfigDescriptorTaglibsInner(BaseModel):
     """
     RedirectViewServletContextJspConfigDescriptorTaglibsInner
     """ # noqa: E501
-    taglib_location: Optional[StrictStr] = Field(default=None, alias="taglibLocation")
     taglib_uri: Optional[StrictStr] = Field(default=None, alias="taglibURI")
-    __properties: ClassVar[List[str]] = ["taglibLocation", "taglibURI"]
+    taglib_location: Optional[StrictStr] = Field(default=None, alias="taglibLocation")
+    __properties: ClassVar[List[str]] = ["taglibURI", "taglibLocation"]
 
     model_config = ConfigDict(
         populate_by_name=True,
@@ -82,8 +82,8 @@ class RedirectViewServletContextJspConfigDescriptorTaglibsInner(BaseModel):
             return cls.model_validate(obj)
 
         _obj = cls.model_validate({
-            "taglibLocation": obj.get("taglibLocation"),
-            "taglibURI": obj.get("taglibURI")
+            "taglibURI": obj.get("taglibURI"),
+            "taglibLocation": obj.get("taglibLocation")
         })
         return _obj
 
