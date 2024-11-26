9896f0a6e0e3b7df02b26736b8c7996a5f214046
CDO Jenkins <cdo.ops@cisco.com>
2024-11-26 12:08:29 +0000

    Update OpenAPI.yaml from staging

--- cdo-sdk/python/cdo_sdk_python/__init__.py
+++ cdo-sdk/python/cdo_sdk_python/__init__.py
@@ -15,7 +15,7 @@
 """  # noqa: E501
 
 
-__version__ = "1.2.444"
+__version__ = "1.2.445"
 
 # import apis into sdk package
 from cdo_sdk_python.api.ai_assistant_api import AIAssistantApi
--- cdo-sdk/python/cdo_sdk_python/models/redirect_view_servlet_context.py
+++ cdo-sdk/python/cdo_sdk_python/models/redirect_view_servlet_context.py
@@ -41,12 +41,12 @@ class RedirectViewServletContext(BaseModel):
     init_parameter_names: Optional[Dict[str, Any]] = Field(default=None, alias="initParameterNames")
     session_tracking_modes: Optional[List[StrictStr]] = Field(default=None, alias="sessionTrackingModes")
     servlet_names: Optional[Dict[str, Any]] = Field(default=None, alias="servletNames")
+    servlet_registrations: Optional[Dict[str, RedirectViewServletContextServletRegistrationsValue]] = Field(default=None, alias="servletRegistrations")
     effective_major_version: Optional[StrictInt] = Field(default=None, alias="effectiveMajorVersion")
     effective_minor_version: Optional[StrictInt] = Field(default=None, alias="effectiveMinorVersion")
     servlets: Optional[Dict[str, Any]] = None
     server_info: Optional[StrictStr] = Field(default=None, alias="serverInfo")
     servlet_context_name: Optional[StrictStr] = Field(default=None, alias="servletContextName")
-    servlet_registrations: Optional[Dict[str, RedirectViewServletContextServletRegistrationsValue]] = Field(default=None, alias="servletRegistrations")
     filter_registrations: Optional[Dict[str, RedirectViewServletContextFilterRegistrationsValue]] = Field(default=None, alias="filterRegistrations")
     session_cookie_config: Optional[RedirectViewServletContextSessionCookieConfig] = Field(default=None, alias="sessionCookieConfig")
     default_session_tracking_modes: Optional[List[StrictStr]] = Field(default=None, alias="defaultSessionTrackingModes")
@@ -55,7 +55,7 @@ class RedirectViewServletContext(BaseModel):
     virtual_server_name: Optional[StrictStr] = Field(default=None, alias="virtualServerName")
     request_character_encoding: Optional[StrictStr] = Field(default=None, alias="requestCharacterEncoding")
     response_character_encoding: Optional[StrictStr] = Field(default=None, alias="responseCharacterEncoding")
-    __properties: ClassVar[List[str]] = ["sessionTimeout", "classLoader", "majorVersion", "minorVersion", "attributeNames", "contextPath", "initParameterNames", "sessionTrackingModes", "servletNames", "effectiveMajorVersion", "effectiveMinorVersion", "servlets", "serverInfo", "servletContextName", "servletRegistrations", "filterRegistrations", "sessionCookieConfig", "defaultSessionTrackingModes", "effectiveSessionTrackingModes", "jspConfigDescriptor", "virtualServerName", "requestCharacterEncoding", "responseCharacterEncoding"]
+    __properties: ClassVar[List[str]] = ["sessionTimeout", "classLoader", "majorVersion", "minorVersion", "attributeNames", "contextPath", "initParameterNames", "sessionTrackingModes", "servletNames", "servletRegistrations", "effectiveMajorVersion", "effectiveMinorVersion", "servlets", "serverInfo", "servletContextName", "filterRegistrations", "sessionCookieConfig", "defaultSessionTrackingModes", "effectiveSessionTrackingModes", "jspConfigDescriptor", "virtualServerName", "requestCharacterEncoding", "responseCharacterEncoding"]
 
     @field_validator('session_tracking_modes')
     def session_tracking_modes_validate_enum(cls, value):
@@ -173,17 +173,17 @@ class RedirectViewServletContext(BaseModel):
             "initParameterNames": obj.get("initParameterNames"),
             "sessionTrackingModes": obj.get("sessionTrackingModes"),
             "servletNames": obj.get("servletNames"),
-            "effectiveMajorVersion": obj.get("effectiveMajorVersion"),
-            "effectiveMinorVersion": obj.get("effectiveMinorVersion"),
-            "servlets": obj.get("servlets"),
-            "serverInfo": obj.get("serverInfo"),
-            "servletContextName": obj.get("servletContextName"),
             "servletRegistrations": dict(
                 (_k, RedirectViewServletContextServletRegistrationsValue.from_dict(_v))
                 for _k, _v in obj["servletRegistrations"].items()
             )
             if obj.get("servletRegistrations") is not None
             else None,
+            "effectiveMajorVersion": obj.get("effectiveMajorVersion"),
+            "effectiveMinorVersion": obj.get("effectiveMinorVersion"),
+            "servlets": obj.get("servlets"),
+            "serverInfo": obj.get("serverInfo"),
+            "servletContextName": obj.get("servletContextName"),
             "filterRegistrations": dict(
                 (_k, RedirectViewServletContextFilterRegistrationsValue.from_dict(_v))
                 for _k, _v in obj["filterRegistrations"].items()
