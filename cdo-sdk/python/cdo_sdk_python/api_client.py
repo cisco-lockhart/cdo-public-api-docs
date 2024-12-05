dda90e5ced24f368e693f3a1e0a7c446b1d5a595
CDO Jenkins <cdo.ops@cisco.com>
2024-12-05 10:36:29 +0000

    Update OpenAPI.yaml from staging

--- cdo-sdk/python/cdo_sdk_python/api_client.py
+++ cdo-sdk/python/cdo_sdk_python/api_client.py
@@ -88,7 +88,7 @@ class ApiClient:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'OpenAPI-Generator/1.2.498/python'
+        self.user_agent = 'OpenAPI-Generator/1.2.499/python'
         self.client_side_validation = configuration.client_side_validation
 
     def __enter__(self):
