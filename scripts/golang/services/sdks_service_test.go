638ec642c45eed81fd4325a7499dfc5d887b7140
Jake Friedman <156102179+jakfried519@users.noreply.github.com>
2026-01-23 11:49:17 -0500

    Revert "feat: include openapi.yaml in published SDK"

--- scripts/golang/services/sdks_service_test.go
+++ scripts/golang/services/sdks_service_test.go
@@ -43,19 +43,11 @@ var _ = Describe("SdksService", func() {
 
 					output := "test output"
 					return &MockCommandExecutor{output: &output}
-				} else if execCount == 2 {
+				} else {
 					// validate args
 					Expect(name).To(Equal("npx"))
 					Expect(args).To(HaveLen(9))
 					Expect(args).To(Equal([]string{"./node_modules/@openapitools/openapi-generator-cli", "generate", "-i", openapiFile, "-g", "python", "-o", "sdks/python", "--additional-properties=packageName=scc_firewall_manager_sdk,packageVersion=" + version}))
-
-					output := "test output"
-					return &MockCommandExecutor{output: &output}
-				} else {
-					// validate args
-					Expect(name).To(Equal("cp"))
-					Expect(args).To(HaveLen(2))
-					Expect(args).To(Equal([]string{openapiFile, "sdks/python/scc_firewall_manager_sdk/"}))
 
 					output := "test output"
 					return &MockCommandExecutor{output: &output}
@@ -64,29 +56,17 @@ var _ = Describe("SdksService", func() {
 			services.ExecCommand = mockShellCommandFunc
 			err := services.GeneratePythonSdk(openapiFile, version, true)
 			Expect(err).To(BeNil())
-			Expect(execCount).To(Equal(3))
+			Expect(execCount).To(Equal(2))
 		})
 
 		It("should generate a python SDK using a global installation", func() {
 			openapiFile := "test-file"
 			version := "1.0.0"
-			execCount := 0
 			mockShellCommandFunc := func(name string, args ...string) services.CommandExecutor {
-				execCount++
-				if execCount == 1 {
-					// validate args
-					Expect(name).To(Equal("npx"))
-					Expect(args).To(HaveLen(9))
-					Expect(args).To(Equal([]string{"@openapitools/openapi-generator-cli", "generate", "-i", openapiFile, "-g", "python", "-o", "sdks/python", "--additional-properties=packageName=scc_firewall_manager_sdk,packageVersion=" + version}))
-
-					output := "test output"
-					return &MockCommandExecutor{output: &output}
-				} else {
-					// validate args
-					Expect(name).To(Equal("cp"))
-					Expect(args).To(HaveLen(2))
-					Expect(args).To(Equal([]string{openapiFile, "sdks/python/scc_firewall_manager_sdk/"}))
-				}
+				// validate args
+				Expect(name).To(Equal("npx"))
+				Expect(args).To(HaveLen(9))
+				Expect(args).To(Equal([]string{"@openapitools/openapi-generator-cli", "generate", "-i", openapiFile, "-g", "python", "-o", "sdks/python", "--additional-properties=packageName=scc_firewall_manager_sdk,packageVersion=" + version}))
 
 				output := "test output"
 				return &MockCommandExecutor{output: &output}
@@ -94,7 +74,6 @@ var _ = Describe("SdksService", func() {
 			services.ExecCommand = mockShellCommandFunc
 			err := services.GeneratePythonSdk(openapiFile, version, false)
 			Expect(err).To(BeNil())
-			Expect(execCount).To(Equal(2))
 		})
 
 		It("should fail if python SDK generation fails", func() {
@@ -115,40 +94,6 @@ var _ = Describe("SdksService", func() {
 			err := services.GeneratePythonSdk(openapiFile, version, false)
 			Expect(err).ToNot(BeNil())
 			Expect(err).To(Equal(expectedErr))
-		})
-
-		It("should fail if copying the OpenAPI spec fails", func() {
-			openapiFile := "test-file"
-			version := "1.0.0"
-			generationOutput := "generation-output"
-			copyOutput := "copy-output"
-			copyErrMsg := "copy-error"
-			expectedErr := errors.New(fmt.Sprintf("Error: %s.\nCopy OpenAPI Spec Output: %s", copyErrMsg, copyOutput))
-
-			execCount := 0
-			mockShellCommandFunc := func(name string, args ...string) services.CommandExecutor {
-				execCount++
-				if execCount == 1 {
-					// successful SDK generation
-					Expect(name).To(Equal("npx"))
-					Expect(args).To(HaveLen(9))
-					Expect(args).To(Equal([]string{"@openapitools/openapi-generator-cli", "generate", "-i", openapiFile, "-g", "python", "-o", "sdks/python", "--additional-properties=packageName=scc_firewall_manager_sdk,packageVersion=" + version}))
-
-					return &MockCommandExecutor{output: &generationOutput}
-				} else {
-					// failing copy command
-					Expect(name).To(Equal("cp"))
-					Expect(args).To(HaveLen(2))
-					Expect(args).To(Equal([]string{openapiFile, "sdks/python/scc_firewall_manager_sdk/"}))
-
-					return &MockCommandExecutor{err: errors.New(copyErrMsg), output: &copyOutput}
-				}
-			}
-			services.ExecCommand = mockShellCommandFunc
-			err := services.GeneratePythonSdk(openapiFile, version, false)
-			Expect(err).ToNot(BeNil())
-			Expect(err).To(Equal(expectedErr))
-			Expect(execCount).To(Equal(2))
 		})
 	})
 })
